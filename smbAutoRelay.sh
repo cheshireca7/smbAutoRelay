5ffed62ae138e306d2056d1484febd91f7a82d5c
chesire <34421496+chesire-cat@users.noreply.github.com>
2020-09-03 12:01:11 +0200

    Update smbAutoRelay.sh

--- smbAutoRelay.sh
+++ smbAutoRelay.sh
@@ -165,7 +165,7 @@ function checkTargets(){
 		portStatus=$(nc -nvzq 1 $line 445 |& awk '{print $5}')
 		if [ $portStatus != 'open' ];then
 			if [ ! -z $quiet ];then echo -e "\t${yellowColour}[:S]${endColour} Target $line is not alive or has the SMB service disable. Removing from targets...\n"; sleep 0.5; fi
-			grep -v '$line' $targets > $(pwd)/impacket/targets.txt
+			grep -v "$line" $targets > $(pwd)/impacket/targets.txt
 		fi
 	done < $targets
 		
